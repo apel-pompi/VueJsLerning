de168c1193ef52a219e5bdac152763b103f3c38d
apel-pompi <apel.pompi@gmail.com>
2024-10-14 17:19:04 +0600

    custom event

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="home">
+    <navbar></navbar>
+    <allfriend :friends="friends" @delete="deleteFriend"></allfriend>
+    <Online :friends="friends" />
+  </div>
+</template>
+<script>
+import navbar from './components/Navbar'
+import allfriend from './components/AllFriends'
+import Online from './components/OnlineFriends'
+export default {
+  name:'app',
+  data(){
+    return {
+      friends:[
+                {name:'A',online:true},
+                {name:'B',online:true},
+                {name:'C',online:false},
+                {name:'D',online:true},
+                {name:'E',online:false},
+                {name:'F',online:false},
+            ]
+    }
+  },
+  components:{
+    navbar,
+    allfriend,
+    Online
+  },
+  methods:{
+    deleteFriend(payload){
+      this.friends = this.friends.filter(friend => {
+        return friend.name !== payload.name
+      })
+    }
+  }
+
+}
+</script>
+<style>
+#home {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="navbar">
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Setting</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'Navbar',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<style>
+nav{text-align: center;}
+nav ul{padding: 0;}
+nav li{display: inline-block;list-style: none;margin: 0; padding: 8px;}
+</style>
