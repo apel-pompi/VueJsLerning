ebe642c056899d122b40ceaa2877e25319a0b4a0
apel-pompi <apel.pompi@gmail.com>
2024-10-16 19:17:26 +0600

    router value pass

---
+++ src/components/Profile.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="profile">
+        <h2>Profile Page ID: {{ userId }}</h2>
+    </div>
+</template>
+<script>
+export default{
+    name:'Profile',
+    data() {
+        return {
+            userId: this.$route.params.user_id
+        };
+    },
+    methods:{
+        updateId(){
+            this.userId = this.$route.params.user_id
+        }
+    },
+    watch:{
+        $route: 'updateId'
+    }
+}
+</script>
